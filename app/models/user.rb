99ef62b645328fb3eb279f7413f2157117710b10
angelo <angelo91291@gmail.com>
2016-07-30 18:16:16 +0000

    Creacion de perfiles v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_one :profile
+  
+  enum role: [:client, :administrator, :offer]
+			
+	after_initialize :set_default_role
+	
+  after_create :set_default_profile
+  
+  def set_default_profile
+    profile = Profile.new
+    profile.user = self
+    profile.save
+  end
+  
+  def friendly_name
+    self.profile.first_name.nil? ? self.email : self.profile.full_name  
+  end
+  
+  def set_default_role
+    				self.role ||= :client
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722004324) do
+ActiveRecord::Schema.define(version: 20160730175529) do
+
+  create_table "profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone"
+    t.datetime "born_at"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,8 +36,10 @@ ActiveRecord::Schema.define(version: 20160722004324) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
 end
